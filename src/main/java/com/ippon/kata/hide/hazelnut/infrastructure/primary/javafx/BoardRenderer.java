93f36fc0f37a7f0ef803e0df5ffaddc946a037c9
Simon Chareyron <schareyron@ippon.fr>
2023-11-03 11:27:34 +0100

    draw board

--- src/main/java/com/ippon/kata/hide/hazelnut/infrastructure/primary/javafx/BoardRenderer.java
+++ src/main/java/com/ippon/kata/hide/hazelnut/infrastructure/primary/javafx/BoardRenderer.java
@@ -4,8 +4,11 @@ import com.ippon.kata.hide.hazelnut.application.domain.Board;
 import com.ippon.kata.hide.hazelnut.application.domain.Slot;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BoardRenderer extends AbstractRenderer<Board> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(BoardRenderer.class);
 
   @Override
   public void render(GraphicsContext graphicsContext, Board board) {
@@ -14,21 +17,46 @@ public class BoardRenderer extends AbstractRenderer<Board> {
         .forEach(
             (position, slot) -> {
               if (!slot.isOccupied()) {
-                renderEmptySlot(graphicsContext, Color.GRAY, position.x(), position.y());
+                renderEmptySlot(graphicsContext, Color.MAROON, slot, position.x(), position.y());
               } else {
-                renderPieceParcel(
-                    graphicsContext, slot, position.x() * BLOCK_SIZE, position.y() * BLOCK_SIZE);
+                LOGGER.info("Slot {} is occupied", slot);
+                renderPieceParcel(graphicsContext, slot, position.x(), position.y());
               }
             });
   }
 
-  private void renderPieceParcel(GraphicsContext graphicsContext, Slot slot, int y, int x) {}
+  private void renderPieceParcel(GraphicsContext graphicsContext, Slot slot, int x, int y) {
+    renderParcel(graphicsContext, toColor(slot), x, y);
+  }
 
-  private static void renderEmptySlot(GraphicsContext graphicsContext, Color color, int x, int y) {
+  private static Color toColor(Slot slot) {
+    return switch (slot.piece().orElseThrow().color()) {
+      case BLACK -> Color.BLACK;
+      case ORANGE -> Color.ORANGE;
+      case RED -> Color.RED;
+      case GREY -> Color.GRAY;
+      case YELLOW -> Color.YELLOW;
+    };
+  }
+
+  private static void renderEmptySlot(
+      GraphicsContext graphicsContext, Color color, Slot slot, int x, int y) {
+    renderParcel(graphicsContext, color, x, y);
+    if (slot.hasHole()) {
+      final double upperLeftX = (double) x * BLOCK_SIZE + PADDING / 2;
+      final double upperLeftY = (double) y * BLOCK_SIZE + PADDING / 2;
+      double width = BLOCK_SIZE - PADDING;
+      graphicsContext.setFill(Color.BLACK);
+      graphicsContext.strokeOval(upperLeftX, upperLeftY, width, width);
+      graphicsContext.setFill(Color.GRAY);
+      graphicsContext.fillOval(upperLeftX, upperLeftY, width, width);
+    }
+  }
+
+  private static void renderParcel(
+      GraphicsContext graphicsContext, Color color, double x, double y) {
     graphicsContext.setFill(color);
-    graphicsContext.fillRect(
-        (double) x * BLOCK_SIZE, (double) y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    graphicsContext.strokeRect(
-        (double) x * BLOCK_SIZE, (double) y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    graphicsContext.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    graphicsContext.strokeRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
   }
 }
--- src/main/java/com/ippon/kata/hide/hazelnut/infrastructure/primary/javafx/Renderer.java
+++ src/main/java/com/ippon/kata/hide/hazelnut/infrastructure/primary/javafx/Renderer.java
@@ -5,13 +5,11 @@ import javafx.scene.canvas.GraphicsContext;
 public interface Renderer<E> {
   int TEXT_HEIGHT = 20;
   int TEXT_WIDTH = 300;
-  double HALF = 2.0;
-  int LEVEL_Y_OFFSET = 60;
   int WIDTH = 10;
   int HEIGHT = 22;
   int BLOCK_SIZE = 180;
 
-  double PADDING = 20.0;
+  double PADDING = 40.0;
 
   void render(GraphicsContext graphicsContext, E event);
 
